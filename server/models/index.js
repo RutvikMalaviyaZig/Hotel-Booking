e54169baa0dcb005ce6424d7e7aea5a0779fb944
RutvikMalaviyaZig <rutvikm@zignuts.com>
2025-07-23 15:39:30 +0530

    update name

--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,5 @@
 import User from "./User.js";
-import Hotel from "./Hotel.js";
+import Hotel from "./hotel.js";
 import Room from "./Room.js";
 import Booking from "./Booking.js";
 import Admin from "./Admin.js";
